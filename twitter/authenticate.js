8244fc319c0adb1e1224b1878ce57065380f3472
naliuj <naliuj@users.noreplu.github.com>
2016-09-21 09:01:32 -0400

    set up authentication for twitter

---
+++ twitter/authenticate.js
@@ -0,0 +1,13 @@
+var config = require('./config/twitter');
+var Twitter = require('twitter');
+
+// authenticate the app with twitter
+var client = new Twitter({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token_key: config.access_token_key,
+    access_token_secret: config.access_token_secret
+});
+
+// expose the client variable to the app
+module.exports = client;
